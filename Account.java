59a06680cba445981d09c45f6c755fd433fffb0b
tsegazeab <tsetam2000@gmail.com>
2023-10-28 11:19:51 +0300

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,45 @@
+public class Account {
+    protected int accountNumber;
+    protected String accountHolderName;
+    protected double balance;
+
+    public Account(){
+
+    }
+
+    public Account(int accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void checkBalance() {
+        System.out.println("Current balance: $" + balance);
+    }
+
+   
+}
